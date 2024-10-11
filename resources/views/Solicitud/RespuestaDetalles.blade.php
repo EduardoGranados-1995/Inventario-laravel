72058d0fed7d0113f8d941729db3c0ef127c55f8
EduardoGranados-1995 <egranados@inba.gob.mx>
2024-10-11 16:47:38 -0600

    Respuesta Detalle

--- resources/views/Solicitud/RespuestaDetalles.blade.php
+++ resources/views/Solicitud/RespuestaDetalles.blade.php
@@ -6,8 +6,41 @@
     <h2>Detalles de la Solicitud</h2>
   </div>
 
-  HOLAAAAAA {{ $solicitud->nombre }}
+  <div align="right">
+    <a href="{{ url('/solicitud-respuesta')}}" class="btn btn-warning m-4"><i class="fa fa-angle-double-left" aria-hidden="true"></i>&nbsp;Regresar</a>
+  </div>
 
- 
+
+    <table class="table table-bordered">
+      <thead class="text-center">
+        <tr>
+          <th colspan="2">Listado de Solicitudes Recibidas</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <strong>Nombre del Solicitante: </strong>{{ $solicitud->nombre }}
+          </td>
+          <td>
+            <strong>Centro de Trabajo: </strong>{{ $solicitud->ct_id }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <strong>Producto Solicitado: </strong>{{ $solicitud->producto_id }}
+          </td>
+          <td>
+            <strong>Cantidad Solicitada: </strong>{{ $solicitud->cantidad }}
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <strong>Descripcion: </strong>{{ $solicitud->descripcion }}
+          </td>
+        </tr>
+        <tr class="text-right">
+      </tbody>
+    </table> 
 </div>
 @endsection
